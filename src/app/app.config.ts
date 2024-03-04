9e54ca71173a8e02a2b786450d828747ea5d5995
Takács Balázs <takacsbalazs2014@gmail.com>
2024-03-04 11:54:24 +0100

    firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"activity-tracker-angular","appId":"1:619774498203:web:53a8887acf30108e087d3e","databaseURL":"https://activity-tracker-angular-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"activity-tracker-angular.appspot.com","apiKey":"AIzaSyC7z5joyytOavCG7eHduZGazwHNIKD0tZU","authDomain":"activity-tracker-angular.firebaseapp.com","messagingSenderId":"619774498203"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideDatabase(() => getDatabase()))]
 };
